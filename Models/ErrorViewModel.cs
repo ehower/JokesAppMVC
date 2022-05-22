a01611250373744d12dbc41ba03dab3d3a3c8dfc
ethan <erh3429@rit.edu>
2022-05-22 16:17:02 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JokesAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
